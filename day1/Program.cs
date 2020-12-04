9d4a5dd82ca4d04ea45a58fefe9dc1740ba2f452
Matthijs Hofstra <matthijs.hofstra@ecare.nl>
2020-12-04 21:12:34 +0100

    Solution to day 1

---
+++ day1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File
+                .ReadAllLines("input.txt")
+                .SelectMany(s => uint.TryParse(s, out var n) ? new[] { n } : Array.Empty<uint>());
+            var numberSet = new HashSet<uint>(numbers);
+
+            var firstResult = 0u;
+            var secondResult = 0u;
+            foreach (var a in numberSet)
+            {
+                foreach (var b in numberSet)
+                {
+                    var sum = a + b;
+                    if (sum == 2020)
+                    {
+                        firstResult = a * b;
+                    }
+                    else if (sum < 2020 && numberSet.Contains(2020 - sum))
+                    {
+                        secondResult = a * b * (2020 - sum);
+                    }
+                }
+            }
+
+            Console.WriteLine("First result: {0}\nSecond result: {1}", firstResult, secondResult);
+        }
+    }
+}
